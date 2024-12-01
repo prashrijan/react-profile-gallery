f8590601d20416ff45df31795481e8692e387e7c
Prashrijan <shresthaprashrijan@gmail.com>
2024-12-02 09:19:07 +1100

    react profile gallery

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper flex flex-col items-center py-10">
+        <h2 className="text-3xl font-bold">Profile Summary</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
